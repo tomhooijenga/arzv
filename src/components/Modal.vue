2c1f7b457629c0a53ca6f0edda924464de5f58b8
Tom Hooijenga <tomhooijenga@gmail.com>
2020-11-04 19:57:37 +0100

    Use teleport for modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
-  {{ show }}
-  <div v-if="show"
-       class="uk-modal uk-open uk-flex"
-       @click.self="$emit('close')">
-    <div class="uk-modal-dialog uk-margin-auto-vertical uk-modal-body">
-      <slot></slot>
+  <teleport to="body">
+    <div v-if="show"
+         class="uk-modal uk-open uk-flex"
+         @click.self="$emit('close')">
+      <div class="uk-modal-dialog uk-margin-auto-vertical uk-modal-body">
+        <slot></slot>
+      </div>
     </div>
-  </div>
+  </teleport>
 </template>
 
 <script lang="ts">
