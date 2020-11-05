437b776adc2df08382f0c64362fa765679d16267
Tom Hooijenga <tomhooijenga@gmail.com>
2020-11-05 20:36:31 +0100

    Add modal animation

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,14 @@
 <template>
   <teleport to="body">
-    <div v-if="show"
-         class="uk-modal uk-open uk-flex"
-         @click.self="$emit('close')">
-      <div class="uk-modal-dialog uk-margin-auto-vertical uk-modal-body">
-        <slot></slot>
+    <transition name="fade">
+      <div v-if="show"
+           class="uk-modal uk-open uk-flex"
+           @click.self="$emit('close')">
+        <div class="uk-modal-dialog uk-margin-auto-vertical uk-modal-body">
+          <slot></slot>
+        </div>
       </div>
-    </div>
+    </transition>
   </teleport>
 </template>
 
@@ -21,3 +23,19 @@ export default defineComponent({
   emits: ['close']
 })
 </script>
+
+<style lang="scss">
+.uk-modal {
+  opacity: initial;
+
+  &.fade-enter-active,
+  &.fade-leave-active {
+    transition: opacity 0.2s ease-in-out;
+  }
+
+  &.fade-enter-from,
+  &.fade-leave-to {
+    opacity: 0;
+  }
+}
+</style>
