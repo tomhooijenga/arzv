319019151c8f6eabf7e21006f3d67196dafc86f8
Tom Hooijenga <tomhooijenga@gmail.com>
2020-11-11 14:43:21 +0100

    Skip wait & client claim for service worker

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   pwa: {
     name: 'ARZV Boten Afschrijven',
     themeColor: '#e52620',
-    msTileColor: '#e52620'
+    msTileColor: '#e52620',
+    workboxOptions: {
+      clientsClaim: true,
+      skipWaiting: true
+    }
   }
 }
