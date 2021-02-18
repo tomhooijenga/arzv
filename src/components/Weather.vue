1dc91f34b2809398242fafe019bc5775f094721d
thooijenga <thooijenga@snelstart.nl>
2021-02-18 20:28:31 +0100

    Add skeleton loader for weather

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,40 +1,55 @@
 <template>
-  <section class="weather-banner" :class="{'ban-active': weather && weather.ban.active}">
-    <transition name="fade">
-      <section v-if="weather" class="uk-flex uk-container uk-text-nowrap weather">
-        <span class="uk-flex uk-flex-auto uk-flex-middle uk-flex-center">
-          <Icon class="icon" name="thermostat"/>
-          <span>
-            {{ weather.temperature.toFixed(0) }}&deg;
-          </span>
-        </span>
+  <section :class="{'ban-active': weather && weather.ban.active}" class="weather-banner uk-background-muted">
+    <div class="uk-flex uk-container uk-flex-between uk-text-nowrap weather">
+      <weather-item :loading="!weather" icon="thermostat">
+        <template v-slot:skeleton>
+          1&deg;
+        </template>
+        <template v-slot>
+          {{ weather.temperature.toFixed(0) }}&deg;
+        </template>
+      </weather-item>
 
-          <span class="uk-flex uk-flex-auto uk-flex-middle uk-flex-center">
-          <Icon class="icon" name="windsock"/>
+      <weather-item :loading="!weather" icon="windsock">
+        <template v-slot:skeleton>
+          1 <small>Bft</small>
+        </template>
+        <template v-slot>
           <span>
-            {{ weather.wind }} <small>Bft</small>
+            {{ weather.wind }} <small class="uk-text-light">Bft</small>
           </span>
-        </span>
+        </template>
+      </weather-item>
 
-          <span class="uk-flex uk-flex-auto uk-flex-middle uk-flex-center">
-          <Icon class="icon" name="air"/>
+      <weather-item :loading="!weather" icon="air">
+        <template v-slot:skeleton>
+          1 <small>Bft</small>
+        </template>
+        <template v-slot>
           <span>
-            {{ weather.gusts }} <small>Bft</small>
+            {{ weather.gusts }} <small class="uk-text-light">Bft</small>
           </span>
-        </span>
+        </template>
+      </weather-item>
 
-        <span class="uk-flex uk-flex-auto uk-flex-middle uk-flex-center uk-text-warning"
-              v-if="weather.ban.active"
-                @click="showBan = !showBan" >
-          <Icon class="icon" name="warning"/>
-          <span>Vaarverbod</span>
+      <weather-item v-if="weather?.ban.active"
+                    :loading="false"
+                    class="uk-text-warning"
+                    icon="warning"
+                    @click="showBan = !showBan">
+        <template v-slot:skeleton>
+          Vaarverbod
+          <Icon name="expand_more"/>
+        </template>
+        <template v-slot>
+          Vaarverbod
           <Icon :name="showBan ? 'expand_less' : 'expand_more'"
                 class="icon"/>
-        </span>
-      </section>
-    </transition>
+        </template>
+      </weather-item>
+    </div>
 
-    <div v-if="showBan" class="ban-info">
+    <div v-if="showBan" class="ban-info uk-section-muted">
       <div class="uk-container uk-text-center uk-text-warning uk-margin-top uk-margin-bottom">
         Er geldt een vaarverbod vanaf {{ $formatDate(weather.ban.start, 'PP p') }}
       </div>
@@ -45,10 +60,14 @@
 <script>
 import { defineComponent } from 'vue'
 import { useWeather } from '@/effects/use-weather'
-import Icon from '@/components/Icon'
+import WeatherItem from '@/components/WeatherItem'
+import Icon from '@/components/Icon.vue'
 
 export default defineComponent({
-  components: { Icon },
+  components: {
+    WeatherItem,
+    Icon
+  },
   setup () {
     const { weather, showBan } = useWeather()
 
@@ -64,17 +83,14 @@ export default defineComponent({
 @import "@/assets/theme";
 
 .weather-banner {
-  border-bottom: 1px solid transparent;
-  background: white;
+  border-bottom: 1px solid $global-border;
   z-index: 1;
   transition: border-bottom-color .2s linear;
   height: 2rem;
-
+  position: relative;
 }
 
 .ban-active {
-  position: sticky;
-  top: 0;
   border-bottom-color: $global-warning-background;
 }
 
@@ -87,14 +103,8 @@ export default defineComponent({
   top: 100%;
   left: 0;
   right: 0;
-  background: white;
   border-bottom: 1px solid $global-warning-background;
   z-index: 2;
   box-shadow: 0 2px 2px 0 rgb(0, 0, 0, 0.08)
-}
-
-.icon {
-  font-size: 1.5rem;
-  margin-right: 0.5rem;
 }
 </style>
---
+++ src/components/WeatherItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition mode="out-in" name="fade" appear>
+    <span v-if="loading"
+          class="uk-flex uk-flex-middle uk-flex-center">
+      <span class="icon skeleton-text"></span>
+      <span class="skeleton-text">
+        <slot name="skeleton"></slot>
+      </span>
+    </span>
+    <span v-else
+          class="uk-flex uk-flex-middle uk-flex-center">
+      <Icon :name="icon" class="icon"/>
+      <slot></slot>
+    </span>
+  </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Icon from '@/components/Icon.vue'
+
+export default defineComponent({
+  components: {
+    Icon
+  },
+  props: {
+    icon: String,
+    loading: Boolean
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  font-size: 1.5rem;
+  width: 1em;
+  height: 1em;
+  margin-right: 0.25rem;
+}
+</style>
